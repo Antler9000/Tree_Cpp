ecf36f058d1b9c7773f6986f02574c12bebea8d7
Antler9000 <ghenakfn20@naver.com>
2025-02-04 01:40:36 +0900

    기초적인 이진탐색트리의 남은 연산(삭제연산 등) 작성
    1.삭제연산 int remove(int target_key)
    일단 구현이 간단하므로 재귀를 이용함. 다만 중위선행자나 중위후속자를 찾는 코드가 더러워서 추후 개선해야함
    
    2.순회연산 void traverse()
    마찬가지로 재귀를 사용해서 전위순회로 구현함.
    
    3.소멸자 ~tree()
    같이 새로 작성한 아래의 '순회삭제연산'을 이용하도록 함. 다만 해당 연산에 delete this가 들어가면 소멸자와 순회삭제연산이 서로를 호출하며 반복되어서 노드인 head만 delete하는 것으로 작성해둠. 소멸 및 동적 해제에 대해서 추후 고민해야할듯.
    
    4.순회삭제연산 void traverse_remove()
    마찬가지로 재귀를 통해서 후위순회로 밑에서 부터 수동으로 delete를 해나가는 것으로 구현함. 위 소멸자에서 언급한 문제로 인해서 소멸 및 동적 해제에 대해서 추후 고민해야함.

--- 1.기초적인 이진탐색트리/basic_tree.cpp
+++ 1.기초적인 이진탐색트리/basic_tree.cpp
@@ -28,7 +28,26 @@ public:
 	}
 
 	~tree() {
+		traverse_remove();
+	}
 
+	void traverse() {
+		if (head == NULL) {
+			cout << "cannot traverse. head is NULL." << endl;
+			return;
+		}
+		cout << "node key : " << head->key << " / node data : " << head->data << endl;
+		if (head->lchild != NULL) head->lchild->traverse();
+		if (head->rchild != NULL) head->rchild->traverse();
+	}
+
+	void traverse_remove() {
+		if (head != NULL) {
+			if (head->lchild != NULL) head->lchild->traverse_remove();
+			if (head->rchild != NULL) head->rchild->traverse_remove();
+			delete head;
+			head = NULL;
+		}
 	}
 
 	int search(int target_key) {
@@ -53,11 +72,11 @@ public:
 		}
 		else {
 			if (new_key < head->key) {
-				head->lchild = new tree();
+				if(head->lchild == NULL) head->lchild = new tree();
 				head->lchild->insert(new_key, new_data);
 			}
 			else if(head->key < new_key) {
-				head->rchild = new tree();
+				if (head->rchild == NULL) head->rchild = new tree();
 				head->rchild->insert(new_key, new_data);
 			}
 			else {
@@ -67,11 +86,75 @@ public:
 	}
 
 	int remove(int target_key) {
-		if (head != NULL) {
+		if (head == NULL) {
 			cout << "cannot remove! tree is emptied!" << endl;
 			return -1;
 		}
-
+		else {
+			if (target_key < head->key) {
+				if (head->lchild->head->lchild == NULL && head->lchild->head->rchild == NULL) {
+					delete head->lchild;
+					head->lchild = NULL;
+				}
+				else {
+					head->lchild->remove(target_key);
+				}
+			}
+			else if (head->key < target_key) {
+				if (head->rchild->head->lchild == NULL && head->rchild->head->rchild == NULL) {
+					delete head->rchild;
+					head->rchild = NULL;
+				}
+				else {
+					head->rchild->remove(target_key);
+				}
+			}
+			else {
+				if (head->lchild != NULL && head->rchild != NULL) {			//두 자식 모두 있는 경우엔, 중위선행자와 중위후속자 중에서 중위후속자를 없앰(오른쪽 자식 트리에서 가장 왼쪽 노드)
+					tree* previous_ptr = NULL;
+					tree* traverse_ptr = head->rchild;
+					while (traverse_ptr->head->lchild != NULL) {
+						previous_ptr = traverse_ptr;
+						traverse_ptr = traverse_ptr->head->lchild;
+					}
+					if (previous_ptr != NULL) previous_ptr->head->lchild = traverse_ptr->head->rchild;
+					else this->head->rchild = traverse_ptr->head->rchild;
+					head->key = traverse_ptr->head->key;
+					head->data = traverse_ptr->head->data;
+					delete traverse_ptr;
+				}
+				else if (head->lchild == NULL && head->rchild != NULL) {
+					tree* previous_ptr = NULL;
+					tree* traverse_ptr = head->rchild;
+					while (traverse_ptr->head->lchild != NULL) {
+						previous_ptr = traverse_ptr;
+						traverse_ptr = traverse_ptr->head->lchild;
+					}
+					if (previous_ptr != NULL) previous_ptr->head->lchild = traverse_ptr->head->rchild;
+					else this->head->rchild = traverse_ptr->head->rchild;
+					head->key = traverse_ptr->head->key;
+					head->data = traverse_ptr->head->data;
+					delete traverse_ptr;
+				}
+				else if (head->lchild != NULL && head->rchild == NULL) {
+					tree* previous_ptr = NULL;
+					tree* traverse_ptr = head->lchild;
+					while (traverse_ptr->head->rchild != NULL) {				//중위후속자 노드를 제거하면서 생기는 빈자리로, 해당 중위후속자의 우측 자식을 당겨 놓기 위함(더러운 표현은 나중에 곤치기로) 
+						previous_ptr = traverse_ptr;
+						traverse_ptr = traverse_ptr->head->rchild;
+					}
+					if (previous_ptr != NULL) previous_ptr->head->rchild = traverse_ptr->head->lchild;
+					else this->head->lchild = traverse_ptr->head->lchild;
+					head->key = traverse_ptr->head->key;
+					head->data = traverse_ptr->head->data;
+					delete traverse_ptr;
+				}
+				else {
+					delete head;
+					head = NULL;
+				}
+			}
+		}
 	}
 };
 int main() {
@@ -79,8 +162,38 @@ int main() {
 	test_tree.insert(5, 1515);
 	test_tree.insert(7, 2727);
 	test_tree.insert(3, 1313);
-	cout << "searched data is " << test_tree.search(5) << endl;
-	cout << "searched data is " << test_tree.search(7) << endl;
-	cout << "searched data is " << test_tree.search(3) << endl;
+	test_tree.insert(4, 2424);
+	test_tree.insert(6, 3636);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+
+	
+	test_tree.remove(7);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+
+	test_tree.remove(5);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+
+	test_tree.remove(6);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+
+	test_tree.remove(4);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+
+	test_tree.remove(3);
+	cout << "traverse" << endl;
+	test_tree.traverse();
+	
+
+	
+	test_tree.traverse_remove();
+	cout << "traverse" << endl;
+	test_tree.traverse();
+	
+
 	return 0;
 }
