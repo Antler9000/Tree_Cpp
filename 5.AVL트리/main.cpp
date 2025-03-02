55f291bf076925fdf4eac54242f827047c72a044
Antler9000 <ghenakfn20@naver.com>
2025-03-02 10:55:40 +0900

    시작을 위한 헤더, 구현 파일 세팅
    일단 splay_tree의 헤더파일과 구현파일을 그대로 가져와 이름만 바꾸었음.
    
    AVL_tree의 헤더파일과 구현파일을 수정하기 이전에, 우선 BST_while에서 이진 탐색 트리를 템플릿화하여 BST_node외에 AVL_node도 처리할 수 있도록 해야함.

---
+++ 5.AVL트리/main.cpp
@@ -0,0 +1,66 @@
+#include "AVL_tree.h"
+
+int main() {
+	AVL_tree& test_tree = *(new AVL_tree());
+	test_tree.insert(5, 1515);
+	test_tree.insert(7, 2727);
+	test_tree.insert(3, 1313);
+	test_tree.insert(4, 2424);
+	test_tree.insert(6, 3636);
+	test_tree.insert(1, 5151);
+	test_tree.insert(8, 5858);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 8" << endl;
+	test_tree.retrieve(8);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 4" << endl;
+	test_tree.retrieve(4);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 3" << endl;
+	test_tree.retrieve(3);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 3" << endl;
+	test_tree.retrieve(3);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 7" << endl;
+	test_tree.retrieve(7);
+	test_tree.preorder_print();
+
+	cout << "retrieve test 5" << endl;
+	test_tree.retrieve(5);
+	test_tree.preorder_print();
+
+
+	AVL_tree& copied_test_tree = *(new AVL_tree());
+	copied_test_tree.copy_from(&test_tree);
+	copied_test_tree.preorder_print();
+
+	test_tree.remove(7);
+	test_tree.preorder_print();
+
+	test_tree.remove(3);
+	test_tree.preorder_print();
+
+	test_tree.remove(5);
+	test_tree.preorder_print();
+
+	test_tree.remove(4);
+	test_tree.preorder_print();
+
+	test_tree.remove(6);
+	test_tree.preorder_print();
+
+	test_tree.remove(1);
+	test_tree.preorder_print();
+
+	test_tree.remove(8);
+	test_tree.preorder_print();
+
+	delete& test_tree;
+	return 0;
+}
