d8307cd18f347df82bedd1cdbc050ec92297e428
Antler9000 <ghenakfn20@naver.com>
2025-03-06 18:41:55 +0900

    삽입 연산에서 균형 조절 메소드 스켈레톤 작성
    (HOW)
    삽입을 위해서 빈 리프 위치로 내려가는 경로를 스택에 저장해놓아서, 이후 삽입이 끝나고 루트를 향해 역으로 되돌아가며 각 노드의 균형인수를 확인하도록 함.

--- 5.AVL트리/AVL_tree.h
+++ 5.AVL트리/AVL_tree.h
@@ -22,6 +22,54 @@ class AVL_node {
 };
 
 class AVL_tree : public BST_template<AVL_node> {
+protected :
+	void balancing_target_node(AVL_node* target_node) {
+		int left_height = 0;
+		int right_height = 0;
+		if (target_node->lchild != NULL) left_height = target_node->lchild->height_from_leaf;
+		if (target_node->rchild != NULL) right_height = target_node->rchild->height_from_leaf;
+
+		if (left_height - right_height >= 2) {
+			if (target_node->lchild->rchild == NULL) {
+				cout << "LL 회전" << endl;
+			}
+			else if (target_node->lchild->lchild == NULL) {
+				cout << "LR 회전" << endl;
+			}
+			else {
+				if (target_node->lchild->lchild->height_from_leaf > target_node->lchild->rchild->height_from_leaf) {
+					cout << "LL 회전" << endl;
+				}
+				else {
+					cout << "LR 회전" << endl;
+				}
+			}
+		}
+		else if (right_height - left_height >= 2) {
+			if (target_node->rchild->rchild == NULL) {
+				cout << "RL 회전" << endl;
+			}
+			else if (target_node->rchild->lchild == NULL) {
+				cout << "RR 회전" << endl;
+			}
+			else {
+				if (target_node->rchild->lchild->height_from_leaf > target_node->rchild->rchild->height_from_leaf) {
+					cout << "RL 회전" << endl;
+				}
+				else {
+					cout << "RR 회전" << endl;
+				}
+			}
+		}
+	}
+
+	void balancing_all_target_to_root(stack<AVL_node*>* ancester_node_stack) {
+		while (ancester_node_stack->is_empty() == false) {
+			AVL_node* reverse_traverse_node = ancester_node_stack->pop();
+			balancing_target_node(reverse_traverse_node);
+		}
+	}
+
 public :
 	AVL_tree() : BST_template() {}
 
@@ -32,20 +80,31 @@ public :
 		}
 
 		AVL_node* traverse_ptr = head;
+		stack<AVL_node*> ancester_node_stack;
 		while (true) {
 			if (new_key < traverse_ptr->key) {
 				if(traverse_ptr->lchild == NULL){
 					traverse_ptr->lchild = new AVL_node(new_key, new_data);
+					ancester_node_stack.push(traverse_ptr);
+					balancing_all_target_to_root(&ancester_node_stack);
 					return;
 				}
-				else traverse_ptr = traverse_ptr->lchild;
+				else {
+					ancester_node_stack.push(traverse_ptr);
+					traverse_ptr = traverse_ptr->lchild;
+				}
 			}
 			else {
 				if (traverse_ptr->rchild == NULL) {
 					traverse_ptr->rchild = new AVL_node(new_key, new_data);
+					ancester_node_stack.push(traverse_ptr);
+					balancing_all_target_to_root(&ancester_node_stack);
 					return;
 				}
-				else traverse_ptr = traverse_ptr->rchild;
+				else {
+					ancester_node_stack.push(traverse_ptr);
+					traverse_ptr = traverse_ptr->rchild;
+				}
 			}
 		}
 	}
--- 5.AVL트리/main.cpp
+++ 5.AVL트리/main.cpp
@@ -7,8 +7,12 @@ int main() {
 	test_tree.insert(3, 1313);
 	test_tree.insert(4, 2424);
 	test_tree.insert(6, 3636);
-	test_tree.insert(1, 5151);
+	test_tree.insert(2, 5252);
 	test_tree.insert(8, 5858);
+	test_tree.insert(1, 5151);
+	test_tree.insert(9, 1919);
+	test_tree.insert(0, 5050);
+	test_tree.insert(10, 1010);
 	test_tree.preorder_print();
 
 	AVL_tree& copied_test_tree = *(new AVL_tree());
@@ -30,7 +34,7 @@ int main() {
 	test_tree.remove(6);
 	test_tree.preorder_print();
 
-	test_tree.remove(1);
+	test_tree.remove(2);
 	test_tree.preorder_print();
 
 	test_tree.remove(8);
