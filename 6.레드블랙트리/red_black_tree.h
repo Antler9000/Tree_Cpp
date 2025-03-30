2c60c55520ccdf51d56e927329ae42fa64b47045
Antler9000 <ghenakfn20@naver.com>
2025-03-30 23:59:23 +0900

    삽입 메소드의 스켈레톤 작성

--- 6.레드블랙트리/red_black_tree.h
+++ 6.레드블랙트리/red_black_tree.h
@@ -48,21 +48,64 @@ protected :
 		target_node->rchild->color = BLACK;
 	}
 
-	void check_and_deal_with_4_nodes(RedBlackNode* target_node, RedBlackNode* parent_node) {
-		if (is_4_node(target_node)) split_4_node(target_node);
+	void check_and_deal_with_4_nodes(Stack<RedBlackNode*>* route_stack) {
+		RedBlackNode* target_node = route_stack->pop();
+		RedBlackNode* parent_node = route_stack->pop();
 
-		if (parent_node != NULL && parent_node->color == RED) {
-			//TODO : ROTATION AND RECOLORING IS NEEDED!
+		if (is_4_node(target_node)) {
+			if (parent_node != NULL && parent_node->color == RED) {
+				RedBlackNode* grand_parent_node = route_stack->pop();
+				RedBlackNode* great_grand_parent_node = route_stack->pop();
+				if (great_grand_parent_node == NULL) {
+					if (grand_parent_node->lchild == parent_node) {
+						if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, head);
+						else									LR_rotation(target_node, parent_node, head);
+					}
+					else {
+						if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, head);
+						else									RL_rotation(target_node, parent_node, head);
+					}
+				}
+				else {
+					if (great_grand_parent_node->lchild == grand_parent_node) {
+						if (grand_parent_node->lchild == parent_node) {
+							if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+							else									LR_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+						}
+						else {
+							if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+							else									RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+						}
+					}
+					else {
+						if (grand_parent_node->lchild == parent_node) {
+							if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+							else									LR_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+						}
+						else {
+							if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+							else									RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+						}
+					}
+				}
+
+				if (great_grand_parent_node != NULL) route_stack->push(great_grand_parent_node);
+				route_stack->push(grand_parent_node);
+			}
+			else split_4_node(target_node);
 		}
+
+		route_stack->push(parent_node);
+		route_stack->push(target_node);
 	}
 
-	void LL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node);
+	void LL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
 
-	void LR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node);
+	void LR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
 
-	void RL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node);
+	void RL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
 
-	void RR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node);
+	void RR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
 
 public :
 	RedBlackTree() : BST_template() {}
@@ -75,8 +118,10 @@ public :
 		
 
 		RedBlackNode* traverse_ptr = head;
-		RedBlackNode* past_traverse_ptr = NULL;
+		Stack<RedBlackNode*> route_stack;
 		while (true) {
+			route_stack.push(traverse_ptr);
+			check_and_deal_with_4_nodes(&route_stack);
 
 			if (new_key < traverse_ptr->key) {
 				if (traverse_ptr->lchild != NULL) {
