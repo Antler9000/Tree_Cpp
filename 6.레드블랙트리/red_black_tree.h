2e31492f510197dbda61668b0773a35b4f938d05
Antler9000 <ghenakfn20@naver.com>
2025-04-01 23:59:57 +0900

    삽입에서 부모가 3-node인 케이스의 회전 작업 작성

--- 6.레드블랙트리/red_black_tree.h
+++ 6.레드블랙트리/red_black_tree.h
@@ -56,38 +56,8 @@ protected :
 			if (parent_node != NULL && parent_node->color == RED) {
 				RedBlackNode* grand_parent_node = route_stack->pop();
 				RedBlackNode* great_grand_parent_node = route_stack->pop();
-				if (great_grand_parent_node == NULL) {
-					if (grand_parent_node->lchild == parent_node) {
-						if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, head);
-						else									LR_rotation(target_node, parent_node, head);
-					}
-					else {
-						if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, head);
-						else									RL_rotation(target_node, parent_node, head);
-					}
-				}
-				else {
-					if (great_grand_parent_node->lchild == grand_parent_node) {
-						if (grand_parent_node->lchild == parent_node) {
-							if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
-							else									LR_rotation(target_node, parent_node, great_grand_parent_node->lchild);
-						}
-						else {
-							if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
-							else									RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
-						}
-					}
-					else {
-						if (grand_parent_node->lchild == parent_node) {
-							if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
-							else									LR_rotation(target_node, parent_node, great_grand_parent_node->rchild);
-						}
-						else {
-							if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
-							else									RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
-						}
-					}
-				}
+
+				split_4_node_by_rotation(target_node, parent_node, grand_parent_node, great_grand_parent_node);
 
 				if (great_grand_parent_node != NULL) route_stack->push(great_grand_parent_node);
 				route_stack->push(grand_parent_node);
@@ -99,13 +69,88 @@ protected :
 		route_stack->push(target_node);
 	}
 
-	void LL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
+	void split_4_node_by_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode* grand_parent_node, RedBlackNode* great_grand_parent_node) {
+		if (great_grand_parent_node == NULL) {
+			if (grand_parent_node->lchild == parent_node) {
+				if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, head);
+				else									LR_rotation(target_node, parent_node, head);
+			}
+			else {
+				if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, head);
+				else									RL_rotation(target_node, parent_node, head);
+			}
+		}
+		else {
+			if (great_grand_parent_node->lchild == grand_parent_node) {
+				if (grand_parent_node->lchild == parent_node) {
+					if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+					else									LR_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+				}
+				else {
+					if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+					else									RL_rotation(target_node, parent_node, great_grand_parent_node->lchild);
+				}
+			}
+			else {
+				if (grand_parent_node->lchild == parent_node) {
+					if (parent_node->lchild == target_node)	LL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+					else									LR_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+				}
+				else {
+					if (parent_node->lchild == target_node)	RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+					else									RL_rotation(target_node, parent_node, great_grand_parent_node->rchild);
+				}
+			}
+		}
+	}
 
-	void LR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
+	void LL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){
+		grand_parent_node->color = RED;
+		parent_node->color = BLACK;
+		target_node->color = RED;
+		target_node->lchild->color = BLACK;
+		target_node->rchild->color = BLACK;
 
-	void RL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
+		grand_parent_node->lchild = parent_node->rchild;
+		parent_node->rchild = grand_parent_node;
+		grand_parent_node = parent_node;
+	}
 
-	void RR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){}
+	void LR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){
+		target_node->lchild->color = BLACK;
+		target_node->rchild->color = BLACK;
+
+		grand_parent_node->lchild = target_node;
+		parent_node->rchild = target_node->lchild;
+		target_node->lchild = parent_node;
+		LL_rotation(parent_node, target_node, grand_parent_node);
+
+		target_node->rchild->color = RED;
+	}
+
+	void RL_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){
+		target_node->lchild->color = BLACK;
+		target_node->rchild->color = BLACK;
+
+		grand_parent_node->rchild = target_node;
+		parent_node->lchild = target_node->rchild;
+		target_node->rchild = parent_node;
+		RR_rotation(parent_node, target_node, grand_parent_node);
+
+		target_node->lchild->color = RED;
+	}
+
+	void RR_rotation(RedBlackNode* target_node, RedBlackNode* parent_node, RedBlackNode*& grand_parent_node){
+		grand_parent_node->color = RED;
+		parent_node->color = BLACK;
+		target_node->color = RED;
+		target_node->lchild->color = BLACK;
+		target_node->rchild->color = BLACK;
+
+		grand_parent_node->rchild = parent_node->lchild;
+		parent_node->lchild = grand_parent_node;
+		grand_parent_node = parent_node;
+	}
 
 public :
 	RedBlackTree() : BST_template() {}
